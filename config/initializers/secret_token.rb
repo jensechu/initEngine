bc53cdb3207f0606f1597c53c47160ee00bc877e
Jensen Hussey <jensechu@gmail.com>
2013-09-12 10:55:43 -0700

    :new_moon: init on rails y'all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InitEngine::Application.config.secret_key_base = 'fea4ae43aacbfd2a4d2a11d985cac6085113bf42f2848f607b93abc470741f68fb8aa51c67a8caa411b728d9ce79bd36db0945327c51848e42a8e4f87c096610'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InitEngine::Application.config.session_store :cookie_store, key: '_initEngine_session'
